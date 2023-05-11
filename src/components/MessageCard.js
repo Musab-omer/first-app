5ba397b759206a31394ba087b469e4a45366f64f
Musab-omer <mosab.katry@gmail.com>
2023-05-11 08:05:45 +0300

    MessageCard Component
    remove the MessageCard.js content for initial setup

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,20 +1,10 @@
 import React from "react";
-import './MessageCard.css';
+
 
 class MessageCard extends React.Component{
     render(){
-        let cardStyle={
-            backgroundColor:'orange',
-            color:'white'
-        };
         return(
-           
-            <div>
-                   <h1 style={{color:'red',backgroundColor:'lightgreen'}}>I am from MessageCard Component with inline style</h1>
-                   <h1 style={cardStyle}>I am from MessageCard Component with internal style</h1>
-                   <h1 id="h1Demo">I am form MessageCard Component with external style</h1>
-                   <h1 className="custom">I am from MessgeCard Component</h1>
-            </div>
+            <div></div>
         );
     }
 }
