051ddb3e4e536406a34837edd63552976b68c2be
Musab-omer <mosab.katry@gmail.com>
2023-05-11 23:41:20 +0300

    MessageCard Component
    MessageCard using class base component

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 
-class MessageCard extends React.Component{
-    render(){
-        return(
-            <div></div>
+class MessageCard extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="card">
+                    <div className="card-body">
+                        <h3>Hellow form Message Card</h3>
+                        <p>Pariatur nulla et incididunt non excepteur. Ut eiusmod duis est voluptate laborum esse nulla. Voluptate consequat aliquip in laborum cillum officia cupidatat laborum veniam est aliquip amet enim. Nostrud tempor velit et esse ex. Ad veniam dolor ut officia anim.</p>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
