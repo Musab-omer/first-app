59a690c364c0d0a429996b9efedec4acc8839564
Musab-omer <mosab.katry@gmail.com>
2023-05-14 09:19:27 +0300

    Configured the App Componet to use bootstrap
    I imported all the file that I need to configure bootstrap with our website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 import './App.css';
-import MessageCard from "./components/MessageCard";
-import DataCard from "./components/DataCard";
 
+import CountryCards from './components/countryCardsComponent/CountryCards';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
-          <a href='#'>React with CSS Styles</a>
-        </nav>
-        <MessageCard/>
-        <DataCard/>
+       <nav className="navbar navbar-dark bg-secondary">
+        <a href='#' className="navbar-brand text-uppercase font-weight-bold">React With Bootstrap</a>
+       </nav>
+
+       <CountryCards/>
       </header>
     </div>
   );
