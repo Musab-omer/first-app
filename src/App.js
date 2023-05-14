2561c2981f4d51832c946cdb97a9bbb0d7f8cd8c
Musab-omer <mosab.katry@gmail.com>
2023-05-14 12:35:35 +0300

    Initialize project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@
 
 import './App.css';
 
-import CountryCards from './components/countryCardsComponent/CountryCards';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <nav className="navbar navbar-dark bg-secondary">
-        <a href='#' className="navbar-brand text-uppercase font-weight-bold">React With Bootstrap</a>
-       </nav>
-
-       <CountryCards/>
+       
       </header>
     </div>
   );
