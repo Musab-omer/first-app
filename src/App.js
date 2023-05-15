3ee620ed48b40de25a3463cc0ad9e3bb3b3669b3
Musab-omer <mosab.katry@gmail.com>
2023-05-15 10:09:36 +0300

    Added images for landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
-
-import './App.css';
-
+//import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
+//import LandingPage from "./components/LandingPage";
+import LandingPage2 from "./components/LandignPage2";
+import CountryCards from "./components/counrties/CountryCards";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <Navbar2/>
+       <LandingPage2/>
+       <CountryCards/>
+       <Contact/>
       </header>
     </div>
   );
