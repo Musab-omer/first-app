35fc11aab7ff7ad9956e419eeef7549815b0ac2c
Musab-omer <mosab.katry@gmail.com>
2023-05-17 08:29:10 +0300

    Initialize navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,26 @@ class Navbar extends React.Component {
         return (
             <React.Fragment>
                 <nav className="navbar navbar-expand-md navbar-dark bg-light">
-                    <a href="/" className="navbar-brand" data-toggler="collapse" data-target="navbarNav">MusabHotel</a>
+                    <a href="/" className="navbar-brand" data-toggle="collapse" data-target="navbarNav">MusabHotel</a>
                     <button className="navbar-toggler">
-                        <span className="navbar-toggler-icon"/>
+                        <span className="navbar-toggler-icon" />
                     </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a href="/" className="nav-link"><i className="fa fa-home"> Home</i></a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link"> <i className="fa fa-globe"> Countries</i></a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link"><i className="fa fa-info"> About</i> </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link"><i className="fa fa-envelope"> Contact</i></a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </React.Fragment>
         );
