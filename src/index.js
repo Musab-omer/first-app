c8bcf6bf3b0d1fbe3679262e90806fc896684444
Musab-omer <mosab.katry@gmail.com>
2023-05-17 08:15:34 +0300

    Orgnize the import section

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@
 */
 
 // import '@fortawesome/fontawesome-free/css/fontawesome.css'
-
-import 'mdbootstrap/css/bootstrap.css';
-import  'mdbootstrap/css/mdb.css';
-import './style.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'mdbootstrap/css/bootstrap.css';
+import  'mdbootstrap/css/mdb.css';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
