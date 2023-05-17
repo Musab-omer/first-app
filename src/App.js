14158862030bb938859819c631c92146d4b09c84
Musab-omer <mosab.katry@gmail.com>
2023-05-17 08:17:08 +0300

    Initialize the app file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-//import Navbar from "./components/Navbar";
-import Navbar2 from "./components/Navbar2";
-//import LandingPage from "./components/LandingPage";
-import LandingPage2 from "./components/LandignPage2";
-import CountryCards from "./components/counrties/CountryCards";
-import Contact from "./components/Contact";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Navbar2/>
-       <LandingPage2/>
-       <CountryCards/>
-       <Contact/>
+     
       </header>
     </div>
   );
