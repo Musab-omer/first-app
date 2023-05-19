8060a662bb7dab5c590f122f9f2477eace704981
Musab-omer <mosab.katry@gmail.com>
2023-05-19 16:44:46 +0300

    Navbar intitlized and developed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React from "react";
-
+import logo from '../assets/imgs/HotelLogo1.png';
 class Navbar extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-md navbar-dark bg-light">
-                    <a href="/" className="navbar-brand" data-toggle="collapse" data-target="navbarNav">MusabHotel</a>
-                    <button className="navbar-toggler">
-                        <span className="navbar-toggler-icon" />
+                <nav className="navbar navbar-expand-md navbar-dark bg-secondary fixed-top">
+                    <a href="/#" className="navbar-brand ml-5"><img src={logo} style={{ height: "50px" }} /></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a href="/" className="nav-link"><i className="fa fa-home"> Home</i></a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav m-auto">
+                            <li className="nav-item active">
+                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-home"> Home</i></a>
                             </li>
-                            <li className="nav-item">
-                                <a href="/" className="nav-link"> <i className="fa fa-globe"> Countries</i></a>
+                            <li className="nav-item ml-2">
+                                <a href="/#" className="nav-link text-uppercase"> <i className="fa fa-globe"> Countries</i></a>
                             </li>
-                            <li className="nav-item">
-                                <a href="/" className="nav-link"><i className="fa fa-info"> About</i> </a>
+                            <li className="nav-item ml-2">
+                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-info"> About</i> </a>
                             </li>
-                            <li className="nav-item">
-                                <a href="/" className="nav-link"><i className="fa fa-envelope"> Contact</i></a>
+                            <li className="nav-item ml-2">
+                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-envelope"> Contact</i></a>
                             </li>
                         </ul>
                     </div>
