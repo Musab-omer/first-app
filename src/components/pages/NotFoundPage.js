7f497d2cb34e8040ad1ca840cb573bb2ffbeda7d
Musab-omer <mosab.katry@gmail.com>
2023-05-20 17:27:23 +0300

    add h2 tag to the component

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -4,7 +4,7 @@ class NotFoundPage extends React.Component{
     render(){
         return(
             <React.Fragment>
-
+                <h2>Not found Page ...</h2>
             </React.Fragment>
         );
     }
