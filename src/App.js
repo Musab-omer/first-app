5c76ab33ddbfae44a943b699445d702b0fc54741
Musab-omer <mosab.katry@gmail.com>
2023-05-20 22:11:58 +0300

    configured navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <Navbar />
       </header>
     </div>
   );
