72b703f52cde5b488d98fee38f35a8981a1cbc54
Musab-omer <mosab.katry@gmail.com>
2023-05-20 17:32:08 +0300

    Imported BrowserRouter of react-router-dom package
    we imported this to allow navigation

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ import 'mdbootstrap/css/mdb.min.css';
 import './style.css';
 import 'bootstrap/js/dist/collapse';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
