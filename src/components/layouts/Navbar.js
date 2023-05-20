74f8edbc930e035885034c8c143d4bfea6e3188f
Musab-omer <mosab.katry@gmail.com>
2023-05-20 17:30:30 +0300

    Removed fixed-top style attribute

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-md navbar-dark bg-secondary fixed-top">
+                <nav className="navbar navbar-expand-md navbar-dark bg-secondary">
                     <a href="/#" className="navbar-brand ml-5"><img src={logo} style={{ height: "50px" }} /></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -12,16 +12,16 @@ class Navbar extends React.Component {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto">
                             <li className="nav-item active">
-                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-home"> Home</i></a>
+                                <a href="/" className="nav-link text-uppercase"><i className="fa fa-home"> Home</i></a>
                             </li>
                             <li className="nav-item ml-2">
-                                <a href="/#" className="nav-link text-uppercase"> <i className="fa fa-globe"> Countries</i></a>
+                                <a href="/countries" className="nav-link text-uppercase"> <i className="fa fa-globe"> Countries</i></a>
                             </li>
                             <li className="nav-item ml-2">
-                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-info"> About</i> </a>
+                                <a href="/about" className="nav-link text-uppercase"><i className="fa fa-info"> About</i> </a>
                             </li>
                             <li className="nav-item ml-2">
-                                <a href="/#" className="nav-link text-uppercase"><i className="fa fa-envelope"> Contact</i></a>
+                                <a href="/contact" className="nav-link text-uppercase"><i className="fa fa-envelope"> Contact</i></a>
                             </li>
                         </ul>
                     </div>
