608f12ae2381a816f8168cad21029a178a4c5365
Musab-omer <mosab.katry@gmail.com>
2023-05-20 18:01:00 +0300

    Initialize the branch

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 
-import Footer from "./components/layouts/Footer";
-
-import Navbar from "./components/layouts/Navbar";
-
-// React Router Import
-
-import {Routes,Route} from 'react-router-dom';
-
-//import pages
-import LandingPage from "./components/pages/LandingPage";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Countries from "./components/pages/Countries";
-import NotFoundPage from './components/pages/NotFoundPage';
-
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
-       
-        <Routes>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path="/countries" element={<Countries/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="*" element={<NotFoundPage/>}/>
-        </Routes>
-        <Footer/>
+      
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -10,15 +10,14 @@ import 'mdbootstrap/css/mdb.min.css';
 import './style.css';
 import 'bootstrap/js/dist/collapse';
 
-import { BrowserRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+   
       <App />
-    </Router>
+  
   </React.StrictMode>
 );
 
