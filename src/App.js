580bb1a51b7f8e57a5dfd29f1a9cd9f140543e61
Musab-omer <mosab.katry@gmail.com>
2023-05-20 17:33:03 +0300

    Updated to allow using of router consept

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+
+import Footer from "./components/layouts/Footer";
+
+import Navbar from "./components/layouts/Navbar";
+
+// React Router Import
+
+import {Routes,Route} from 'react-router-dom';
+
+//import pages
+import LandingPage from "./components/pages/LandingPage";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Countries from "./components/pages/Countries";
-import Footer from "./components/layouts/Footer";
-import LandingPage from "./components/pages/LandingPage";
-import Navbar from "./components/layouts/Navbar";
-
+import NotFoundPage from './components/pages/NotFoundPage';
 
 
 
@@ -13,10 +21,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <LandingPage/>
-        <Countries/>
-        <About/>
-        <Contact/>
+       
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path="/countries" element={<Countries/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
         <Footer/>
       </header>
     </div>
