82bde582a64d287f7c1ffc5ece42c66094c9bd41
Musab-omer <mosab.katry@gmail.com>
2023-05-21 04:31:41 +0300

    updated navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
             <React.Fragment>
                 <nav className="navbar navbar-expand-md navbar-dark bg-secondary">
                     <a href="/" className="navbar-brand">React with Props and State</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarlinks" ari-contorl>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarlinks" >
                         <span className="navbar-toggler-icon"/>
                     </button>
                     
