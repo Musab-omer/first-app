986f8986e057d45a1c36682b9c2ea45f8c1e5cb8
Musab-omer <mosab.katry@gmail.com>
2023-05-23 10:44:53 +0300

    Navbar Ready

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,16 +4,20 @@ class Navbar extends React.Component {
     render() {
         return (
             <React.Fragment>
-                {/* <nav className="navbar navbar-expand-md navbar-dark bg-secondary">
-                    <a href="/" className="navbar-brand">React with Props-State and Event Binding</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarlinks" >
-                        <span className="navbar-toggler-icon"/>
+                <nav className="navbar navbar-expand-md navbar-dark bg-success">
+                    <a href="/" className="navbar-brand">React with Form Binding</a>
+                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    
-                    
-                </nav> */}
-
-
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Home</a>
+                                <a href="/" className="nav-link">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
             </React.Fragment>
         );
