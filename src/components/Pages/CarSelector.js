54afabc6ac71a6556d33be522868c0882091c9ba
Musab-omer <mosab.katry@gmail.com>
2023-05-23 13:29:44 +0300

    Initial commit

---
+++ src/components/Pages/CarSelector.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class CarSelector extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Car Selector </h2>
+            </React.Fragment>
+        );
+    }
+}
+export default CarSelector;
