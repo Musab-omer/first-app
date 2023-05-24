70dd7722e44544747eba822e8d6efee5086664b3
Musab-omer <mosab.katry@gmail.com>
2023-05-24 13:39:01 +0300

    Initiated Navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,23 +4,28 @@ class Navbar extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-md navbar-dark bg-secondary">
-                    <a href="/" className="navbar-brand">React with Form Binding</a>
-                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <a href="/" className="navbar-brand">Online Application Form</a>
+                    <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#mainNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse" id="mainNavbar">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a href="/" className="nav-link">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a href="/" className="nav-link">About</a>
+                            <a href="/" className="nav-link">Personal</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Education</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Experience</a>
                             </li>
                         </ul>
                     </div>
                 </nav>
-
             </React.Fragment>
         );
     }
