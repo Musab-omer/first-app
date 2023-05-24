1a4c229d50b43f37743a192b4fadd1b87bde8bf0
Musab-omer <mosab.katry@gmail.com>
2023-05-24 12:58:45 +0300

    changed component name

---
+++ src/components/Pages/applyForJob.js
@@ -0,0 +1,15 @@
+import { Component, Fragment } from "react".
+
+class applyForJob extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+export default applyForJob;
