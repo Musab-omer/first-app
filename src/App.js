3bd9bf9da9e1f62949c4d93b43a06d7872710d6a
Musab-omer <mosab.katry@gmail.com>
2023-05-24 12:50:31 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import SMSApp from './components/Pages/SMSApp';
-import CarSelector from './components/Pages/CarSelector';
-import MessageCard from './components/Pages/MessageCard';
-
 import Navbar from "./components/layouts/Navbar";
-import Register from './components/Pages/Register';
-import RegisterNew from './components/Pages/RegisterNew';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        {/* <MessageCard/> */}
-        {/* <CarSelector /> */}
-        {/* <SMSApp/> */}
-        {/* <Register/> */}
-
-        <RegisterNew/>
       </header>
     </div>
   );
