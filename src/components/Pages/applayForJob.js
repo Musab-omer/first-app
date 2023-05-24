b1539a9dc6bd3821b65c8db554abfdd55c09cdee
Musab-omer <mosab.katry@gmail.com>
2023-05-24 12:56:12 +0300

    SetUp initial configuration

---
+++ src/components/Pages/applayForJob.js
@@ -0,0 +1,15 @@
+import {Component,Fragment} from "react".
+
+class applayForJob extends Component{
+    constructor(props){
+        super(props);
+    }
+   render(){
+    return(
+        <Fragment>
+
+        </Fragment>
+    );
+   }
+}
+export default applayForJob;
