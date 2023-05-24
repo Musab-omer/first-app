9ab965de081fba1e0ee408fecf9e454319bb63af
Musab-omer <mosab.katry@gmail.com>
2023-05-24 13:39:56 +0300

    configured ApplyForm Job component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from "./components/layouts/Navbar";
+import ApplyForJob from "./components/Pages/applyForJob";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+
+        <ApplyForJob />
       </header>
     </div>
   );
