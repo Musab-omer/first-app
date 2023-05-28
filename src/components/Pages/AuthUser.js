67042f0bf26f347b01f92dd2d7eb917e8d0aff60
Musab-omer <mosab.katry@gmail.com>
2023-05-28 19:46:25 +0300

    Login and Logout senario

---
+++ src/components/Pages/AuthUser.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class AuthUser extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    logIn = () => {
+        this.setState(
+            {
+                isLoggedIn: true
+            }
+        );
+    }
+
+    logOut = () => {
+        this.setState({
+            isLoggedIn: false
+        });
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body">
+                                    <button className="btn btn-success" onClick={this.logIn}>LogIn</button>
+                                    <button className="btn btn-warning" onClick={this.logOut}>LogOut</button>
+                                    {
+                                        this.state.isLoggedIn ?
+                                            <p className="h3">Hello! Welcome User</p>
+                                            :
+                                            <p className="h3">Hello! Welcome Guest </p>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+export default AuthUser;
