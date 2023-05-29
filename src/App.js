fc9de572a099117cadbe995cf3fcb6b7483aefa8
Musab-omer <mosab.katry@gmail.com>
2023-05-29 17:36:45 +0300

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import AuthUser from './components/Pages/AuthUser';
-import Customers from './components/Pages/Customers';
-import EmployeeCard from './components/Pages/EmployeeCard';
-import Hobies from './components/Pages/Hobies';
-import HobiesRadio from './components/Pages/HobiesRadio';
+
 import Navbar from "./components/layouts/Navbar";
 
 
@@ -13,11 +9,7 @@ function App() {
       <header className="App-header">
         <Navbar />
 
-       {/* <AuthUser/> */}
-       {/* <Hobies/> */}
-       {/* <HobiesRadio /> */}
-       {/* <EmployeeCard/> */}
-       <Customers/>
+       
       </header>
     </div>
   );
