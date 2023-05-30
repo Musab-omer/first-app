eee01e8d0343531c3406c37f30b7463b7e9e9731
Musab-omer <mosab.katry@gmail.com>
2023-05-30 17:40:44 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import MiddleWareCard from './components/Pages/advanse/MiddelwareCard';
-import ParentCoponent from './components/Pages/Basic/ParentCoponent';
 
-import ParentTyping from './components/Pages/mediam/ParentTyping';
 
 
 import Navbar from "./components/layouts/Navbar";
-import ContactApp from './components/Pages/contactApp/ContactApp';
+
 
 
 function App() {
@@ -18,13 +15,7 @@ function App() {
       <header className="App-header">
         <Navbar />
 
-        {/* <ParentCoponent/> */}
-
-        {/* <ParentTyping /> */}
-
-        {/* <MiddleWareCard /> */}
-
-        <ContactApp/>
+       
 
       </header>
     </div>
