19c9c979826a4c1320cb71a40e8a313dc71dba1e
Musab-omer <mosab.katry@gmail.com>
2023-06-02 02:24:23 +0300

    first Desing

--- src/components/Pages/github/github-profile/GithubProfileDetails.js
+++ src/components/Pages/github/github-profile/GithubProfileDetails.js
@@ -9,7 +9,21 @@ class GithubProfileDetails extends React.Component{
     render() {
         return (
             <React.Fragment>
-                
+               <div className="pb-3">
+               <span className="badge badge-info mr-2">1665 Followers</span>
+               <span className="badge badge-success mr-2">18 Repos</span>
+               <span className="badge badge-danger mr-2">58 Gists</span>
+               <span className="badge badge-warning">1 Following</span>
+               </div>
+               <ul className="list-group">
+                <li className="list-group-item">User Name : </li>
+                <li className="list-group-item">Location : </li>
+                <li className="list-group-item">Email : </li>
+                <li className="list-group-item">Compnay : </li>
+                <li className="list-group-item">Blog : </li>
+                <li className="list-group-item">Member Since : </li>
+                <li className="list-group-item">Profile URL : </li>
+               </ul>
             </React.Fragment>
         );
     }
