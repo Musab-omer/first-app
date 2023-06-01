7ef6babb3601dd04f0efb3207fbd52431dd139c5
Musab-omer <mosab.katry@gmail.com>
2023-06-01 22:44:03 +0300

    layout ready

--- src/components/Pages/github/github-profile/GithubSearch.js
+++ src/components/Pages/github/github-profile/GithubSearch.js
@@ -9,7 +9,18 @@ class GithubSearch extends React.Component{
     render() {
         return (
             <React.Fragment>
-                
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <form>
+                              <div className="form-group form-inline">
+                                <input type="text" placeholder="GitHub User Name" className="form-control" />
+                                <button className="btn btn-primary">search</button>
+                              </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
