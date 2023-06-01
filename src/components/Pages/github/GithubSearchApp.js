7700720c793bfa28271f67632af02a98f6786a10
Musab-omer <mosab.katry@gmail.com>
2023-06-01 22:41:21 +0300

    re-orgnaize the content

--- src/components/Pages/github/GithubSearchApp.js
+++ src/components/Pages/github/GithubSearchApp.js
@@ -3,21 +3,27 @@ import GitHubSearch from './github-profile/GithubSearch'
 import GitHubProfile from './github-profile/GithubProfile';
 import GithubRepos from './github-repo/GithubRepos'
 
-class GithubSearchApp extends React.Component{
+class GithubSearchApp extends React.Component {
     constructor(props) {
         super(props);
-        
+
     }
 
     render() {
         return (
             <React.Fragment>
-                <GitHubSearch/>
-               <GitHubProfile/>
-               <GithubRepos/>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col">
+                            <GitHubSearch />
+                            <GitHubProfile />
+                            <GithubRepos />
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
-    
+
 }
 export default GithubSearchApp;
