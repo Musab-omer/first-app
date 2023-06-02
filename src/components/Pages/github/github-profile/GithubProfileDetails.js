a4379d3755fb0380b7b5e234a37db482192c18ec
Musab-omer <mosab.katry@gmail.com>
2023-06-02 03:04:24 +0300

    add some style

--- src/components/Pages/github/github-profile/GithubProfileDetails.js
+++ src/components/Pages/github/github-profile/GithubProfileDetails.js
@@ -10,13 +10,13 @@ class GithubProfileDetails extends React.Component{
         return (
             <React.Fragment>
                <div className="pb-3">
-               <span className="badge badge-info mr-2">1665 Followers</span>
-               <span className="badge badge-success mr-2">18 Repos</span>
-               <span className="badge badge-danger mr-2">58 Gists</span>
-               <span className="badge badge-warning">1 Following</span>
+               <span className="badge badge-pill badge-info mr-2">1665 Followers</span>
+               <span className="badge badge-pill badge-success mr-2">18 Repos</span>
+               <span className="badge badge-pill badge-danger mr-2">58 Gists</span>
+               <span className="badge badge-pill badge-warning">1 Following</span>
                </div>
                <ul className="list-group">
-                <li className="list-group-item">User Name : </li>
+                <li className="list-group-item bg-danger">User Name : </li>
                 <li className="list-group-item">Location : </li>
                 <li className="list-group-item">Email : </li>
                 <li className="list-group-item">Compnay : </li>
