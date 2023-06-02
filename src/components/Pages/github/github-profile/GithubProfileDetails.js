4ab1fb6571e01c7ac3eda397a25203b0ad417714
Musab-omer <mosab.katry@gmail.com>
2023-06-02 22:34:35 +0300

    receive user profile details
    in this component I receive the github user profile data and display them in details format

--- src/components/Pages/github/github-profile/GithubProfileDetails.js
+++ src/components/Pages/github/github-profile/GithubProfileDetails.js
@@ -7,22 +7,23 @@ class GithubProfileDetails extends React.Component{
     }
 
     render() {
+        let{name,location,email,company,blog,created_at,url,followers,following,public_repos,public_gists,html_url}=this.props.profile;
         return (
             <React.Fragment>
                <div className="pb-3">
-               <span className="badge badge-pill badge-info mr-2">1665 Followers</span>
-               <span className="badge badge-pill badge-success mr-2">18 Repos</span>
-               <span className="badge badge-pill badge-danger mr-2">58 Gists</span>
-               <span className="badge badge-pill badge-warning">1 Following</span>
+               <span className="badge badge-pill badge-info mr-2">{followers} Followers</span>
+               <span className="badge badge-pill badge-success mr-2">{public_repos} Repos</span>
+               <span className="badge badge-pill badge-danger mr-2">{public_gists} Gists</span>
+               <span className="badge badge-pill badge-warning">{following} Following</span>
                </div>
                <ul className="list-group">
-                <li className="list-group-item bg-danger">User Name : </li>
-                <li className="list-group-item">Location : </li>
-                <li className="list-group-item">Email : </li>
-                <li className="list-group-item">Compnay : </li>
-                <li className="list-group-item">Blog : </li>
-                <li className="list-group-item">Member Since : </li>
-                <li className="list-group-item">Profile URL : </li>
+                <li className="list-group-item bg-secondary text-white">User Name : {name}</li>
+                <li className="list-group-item">Location : {location}</li>
+                <li className="list-group-item">Email : {email}</li>
+                <li className="list-group-item">Compnay {company}: </li>
+                <li className="list-group-item">Blog : {blog}</li>
+                <li className="list-group-item">Member Since : {created_at}</li>
+                <li className="list-group-item">Profile URL : <a href={html_url} target="_blank">{url}</a> </li>
                </ul>
             </React.Fragment>
         );
