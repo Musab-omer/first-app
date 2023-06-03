99d7d38f7416437a75369ba3b7998d088ca64d1e
Musab-omer <mosab.katry@gmail.com>
2023-06-04 00:09:06 +0300

    set router configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
-import GithubSearchApp from './components/Pages/github/GithubSearchApp';
+
 
 import Navbar from "./components/layouts/Navbar";
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Pages/Home';
+import Employees from './components/Pages/employees/Employees';
+import Stocks from './components/Pages/stocks/Stocks';
+import About from './components/Pages/About';
 class App extends Component {
   constructor(props) {
     super(props);
-
-
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Navbar />
-
-          {/* ----------------GitHub Serach App --------- */}
-          <GithubSearchApp/>
+          
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route exact path='/' element={<Home/>} />
+              <Route exact path='/employees/list' element={<Employees/>}/>
+              <Route exact path='/stocks/list' element={<Stocks/>}/>
+              <Route exact path='/about' Component={About}/>
+            </Routes>
+          </BrowserRouter>
         </header>
       </div>
     );
