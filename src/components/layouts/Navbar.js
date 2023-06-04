ade86d57f7878150d55164070708e1ace7023182
Musab-omer <mosab.katry@gmail.com>
2023-06-04 07:34:22 +0300

    Shadow and fixed-top

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow-lg fixed-top">
                     <a href="/" className="navbar-brand">React Routing</a>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#mainNavbar">
                         <span className="navbar-toggler-icon"></span>
@@ -22,7 +22,7 @@ class Navbar extends React.Component {
                             </li>
                             <li className="nav-item mx-2">
                                 {/* <a href="/stocks/list" className="nav-link">Stocks</a> */}
-                                <Link to="/stoks/list" className="nav-link">Stocks</Link>
+                                <Link to="/stocks/list" className="nav-link">Stocks</Link>
                             </li>
                             <li className="nav-item ">
                                 {/* <a href="/about" className="nav-link">About</a> */}
