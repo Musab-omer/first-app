d5f60e1a583cc4cf6dfe07c5b459ebef83e5db8d
Musab-omer <mosab.katry@gmail.com>
2023-06-04 07:33:53 +0300

    about us

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,18 +1,33 @@
 import React from "react";
+import myImg from '../../assets/imgs/_DSC6232.jpg';
 
-class About extends React.Component{
+class About extends React.Component {
     constructor(props) {
         super(props);
-        
+
     }
 
     render() {
         return (
             <React.Fragment>
-                <h1>About</h1>
+                <div className="container" style={{ marginTop: "100px" }}>
+                    <div className="row">
+                        <div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
+                            
+                                <p className="h2">So , who am I ?</p>
+                                <p className="text-monospace">Have 5+ years of Professional experience in developing Web-based, Client/Server applications, Internet/Intranet applications and Windows-based applications using Microsoft Technologies </p>
+                            
+                        </div>
+                        <div className="col-md-6 alig-item-center">
+                            <div className="text-center">
+                                <img src={myImg} className="img-fluid rounded" style={{height:"400px"}} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
-    
+
 }
 export default About;
