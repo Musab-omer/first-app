3e07fec79cdd12e8c2cf037aa56f1abb01e08c7d
Musab-omer <mosab.katry@gmail.com>
2023-06-04 07:35:25 +0300

    max-width

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component{
        
         return (
            <React.Fragment>
-            <div  style={{backgroundImage:"url('"+imgBackground+"')",backgroundSize:"cover",backgroundRepeat:"no-repeat",maxWidth:"100%",height:"100vh"}}>
+            <div  style={{backgroundImage:"url('"+imgBackground+"')",backgroundSize:"cover",backgroundRepeat:"no-repeat",height:"100vh"}}>
                 <div className="d-flex flex-column align-items-center justify-content-center text-white d-sm-flex" style={{height:"100vh",backgroundColor:"rgba(0,0,0,0.5)"}}>
                     <h3 className="">Ex exercitation reprehenderit labore deserunt</h3>
                     <Link to="/about" className="btn btn-dark btn-sm">Learn More ...</Link>
