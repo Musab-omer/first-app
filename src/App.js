7131644927f88f07304efe5e089fe44d00262f63
Musab-omer <mosab.katry@gmail.com>
2023-06-15 18:05:45 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
+import {Component} from 'react';
 
-
-import Navbar from "./components/layouts/Navbar";
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Home from './components/Pages/Home';
-import Employees from './components/Pages/employees/Employees';
-import Stocks from './components/Pages/stocks/Stocks';
-import About from './components/Pages/About';
-import Employee from './components/Pages/employees/EmployeeDetails';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -19,17 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route exact path='/' element={<Home/>} />
-              <Route exact path='/employees/list' element={<Employees/>}/>
-              <Route exact path='/stocks/list' Component={Stocks}/>
-              <Route exact path='/about' Component={About}/>
-              <Route exact path='employees/:id' Component={Employee}/>
-            </Routes>
-          </BrowserRouter>
         </header>
       </div>
     );
